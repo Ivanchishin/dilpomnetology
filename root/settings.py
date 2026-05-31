78ea6403b76720a7ac326aed819acddf2381328d
Ivanchishin <igor-ivanchishin@list.ru>
2026-05-31 18:39:45 +0300

    Minor changes

--- root/settings.py
+++ root/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@*s(z47-33sbov*o=ru1v3su^5x8*=c+%sp14#7a5+&iip=ax('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
